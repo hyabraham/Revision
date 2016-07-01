5a2d1b2b94797d316eaf724202c6dffcd3c8bcad
Abrahaam <hyalincoln@gmail.com>
2016-07-01 13:03:01 +0200

    ajout UserDAO

--- app/src/main/java/bt/be/revisionapplication/db/DBHelper.java
+++ app/src/main/java/bt/be/revisionapplication/db/DBHelper.java
@@ -18,12 +18,13 @@ public class DBHelper extends SQLiteOpenHelper{
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
 
     }
 }
---
+++ app/src/main/java/bt/be/revisionapplication/model/UserDAO.java
@@ -0,0 +1,15 @@
+package bt.be.revisionapplication.model;
+
+/**
+ * Created by rome03 on 1/07/2016.
+ */
+public class UserDAO {
+
+    public static final String TABLE_USER="user";
+    public static final String COLUMN_ID="_id";
+    public static final String COLUMN_LAST_NAME="last_name";
+    public static final String COLUMN_FIRST_NAME="first_name";
+    public static final String COLUMN_REG_NAT="reg_nat";
+
+    //public static final String CREATE_REQUEST="CREATE TABLE"
+}
